d0b9d1801b2be8746be0197f5c08650546d22c47
Andrés Del Carpio <andresdc@andresdc.net>
2022-11-20 17:50:00 -0500

    Cut down initial new line

--- bin/generate.js
+++ bin/generate.js
@@ -10,9 +10,12 @@ const postList = fs.readdirSync('./posts')
 postList.forEach((el, i) => {
   const fileContents = fs.readFileSync('./posts/' + el, 'utf8')
   const contentObj = matter(fileContents)
+
+  contentObj.content = contentObj.content.slice(1)
   contentObj.data.wordcount = contentObj.content.split(' ').length
   contentObj.id = i
   contentObj.data.slug = el.slice(0, -3)
+
   json.data.push(contentObj)
 })
 
